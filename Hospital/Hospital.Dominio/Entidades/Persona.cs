ba4791003511e8dcb11cfe7f522b7c1038cb8347
Jimmy Sanchez <jimmyelpaisa11@hotmail.com>
2021-11-01 13:54:46 -0500

    Actualización Atributos Persona

--- Hospital/Hospital.Dominio/Entidades/Persona.cs
+++ Hospital/Hospital.Dominio/Entidades/Persona.cs
@@ -4,5 +4,6 @@ namespace Hospital.Dominio.Entidades
     {
         public int Id {get; set;} //Id lo reconoce como Primary Key
         public string Nombre {get; set;}
+        public string Apellidos {get; set;}
     }
 }
