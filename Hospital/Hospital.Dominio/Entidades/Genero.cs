748f3d741d8eb940f753d4c4bcb4550d7c48b718
Jimmy Sanchez <jimmyelpaisa11@hotmail.com>
2021-11-01 18:02:30 -0500

    Se crearon las clases médico, paciente, signo vital y las clases tipo enum como género y tipo de signo

---
+++ Hospital/Hospital.Dominio/Entidades/Genero.cs
@@ -0,0 +1,17 @@
+namespace Hospital.Dominio.Entidades
+{
+    /// <summary>
+    /// Lista de Generos
+    ///</summary>
+
+    public enum Genero
+    {
+        Masculino,
+        Femenino,
+        Bisexual,
+        Intersexual,
+        Pansexual,
+        Transexual,
+        Otro
+    }
+}
---
+++ Hospital/Hospital.Dominio/Entidades/TipoSigno.cs
@@ -0,0 +1,16 @@
+namespace Hospital.Dominio.Entidades
+{
+    /// <summary>
+    /// Lista de Signos Vitales
+    ///</summary>
+
+    public enum TipoSigno
+    {
+        TensionArterial,
+        FrecuenciaCardiaca,
+        FrecuenciaRespiratoria,
+        FrecuenciaOxigeno,
+        Temperatura,
+        Peso
+    }
+}
